a8b4d1f36c5f8fded7f1c8579182544151a2bd97
hindrekhan <hindrek.hannus@tptlive.ee>
2018-11-26 08:45:42 +0200

    abc

---
+++ notebook/notebook/DatabaseService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace notebook
+{
+    public class DatabaseService
+    {
+        SQLiteConnection db;
+
+        public void CreateDatabase()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "mydatabase.db3");
+            db = new SQLiteConnection(dbPath);
+        }
+
+        public void CreateTableWithData()
+        {
+            db.CreateTable<Note>();
+            if (db.Table<Note>().Count() == 0)
+            {
+                var newStock = new Note();
+                newStock.Content = "AAPL";
+                db.Insert(newStock);
+                newStock.Content = "AMD";
+                db.Insert(newStock);
+                newStock.Content = "TESLA";
+                db.Insert(newStock);
+                newStock.Content = "AMAZON";
+                db.Insert(newStock);
+                newStock.Content = "MSDN";
+                db.Insert(newStock);
+            }
+        }
+
+        public void AddNote(Note name)
+        {
+            db.Insert(name);
+        }
+
+        public List<Note> GetAllNotes()
+        {
+            var table = db.Table<Note>();
+            return table.ToList();
+        }
+    }
+}
---
+++ notebook/notebook/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace notebook
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        DatabaseService databaseService;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            databaseService = new DatabaseService();
+            databaseService.CreateDatabase();
+            databaseService.CreateTableWithData();
+            var notes = databaseService.GetAllNotes();
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            listView.Adapter = new CustomAdapter(this, notes);
+
+            var button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Note note = new Note();
+            var input = FindViewById<EditText>(Resource.Id.input);
+
+            note.Content = input.Text;
+
+            databaseService.AddNote(note);
+
+            var notes = databaseService.GetAllNotes();
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            listView.Adapter = new CustomAdapter(this, notes);
+        }
+    }
+}
